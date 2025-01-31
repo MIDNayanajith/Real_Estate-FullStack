d5c1f8635e3f9ef4e70dc1b0599a17ea9f573b90
MIDNayanajith <isuranga_2020736@fot.ruh.ac.lk>
2025-01-31 23:45:03 +0530

    frontend

---
+++ Client/src/components/Companies/Companies.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./companies.css";
+const Companies = () => {
+  return (
+    <section className="c-wrapper">
+      <div className="paddings innserWidth flexCenter c-container">
+        <img src="./prologis.png" alt="company_logo1" />
+        <img src="./tower.png" alt="company_logo2" />
+        <img src="./equinix.png" alt="company_logo3" />
+        <img src="./realty.png" alt="company_logo4" />
+      </div>
+    </section>
+  );
+};
+
+export default Companies;
